f290da553379018d94ac27053ce7f8a69a790f1b
dev1980 <brham1980@gmail.com>
2019-11-21 23:24:44 +0545

    Establish relationship between attended events attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_many :attendings, foreign_key: :event_id
+  has_many :attendees, through: :attendings, source: :attendee
 end
