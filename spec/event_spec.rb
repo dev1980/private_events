4c7600e7957cdd8c3488b91a29f8200457189172
Miguel Prada <mapra99@gmail.com>
2019-11-22 11:07:51 -0500

    added unit tests for models

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -27,4 +27,33 @@ RSpec.describe Event, :type => :model do
       expect(event.valid?).to eq(false)
     end
   end
+
+  context "with 1 event and some users" do
+    it "checks if an event must have one creator" do
+      user = User.create(name: 'Dev')
+      event = user.created_events.create(title: 'This is a test title',
+                                        description: 'This is a test description',
+                                        date: Date.today,
+                                        location: 'Nepal')
+      expect(event.creator.name).to eq('Dev')
+    end
+
+    it "checks if an event can have multiple attendees" do
+      user = User.create(name: 'Dev')
+      event = user.created_events.create(title: 'This is a test title',
+                                        description: 'This is a test description',
+                                        date: Date.today,
+                                        location: 'Nepal')
+      
+      attendee1 = User.create(name: 'Miguel')
+      attendee2 = User.create(name: 'Bhram')
+
+      
+      attending1 = attendee1.attendings.create(event_id: event.id)
+      attending2 = attendee2.attendings.create(event_id: event.id)
+
+      expect(event.attendees).to eq([attendee1, attendee2])
+      expect(event.attendings).to eq([attending1, attending2])
+    end
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -12,4 +12,42 @@ RSpec.describe User, :type => :model do
       expect(user.valid?).to eq(true)
     end
   end
+
+  context "with 1 user that created 1 event" do
+
+    it "checks if a user can create mulitple events" do
+      user = User.create(name: 'Dev')
+      event1 = user.created_events.create(title: 'This is a test title',
+                                        description: 'This is a test description',
+                                        date: Date.today,
+                                        location: 'Nepal')
+
+      event2 = user.created_events.create(title: 'This is a test title for second event',
+                                          description: 'This is a test description for second event',
+                                          date: Date.today,
+                                          location: 'Bogota')
+
+      expect(user.created_events).to eq([event2, event1])
+    end
+
+    it "checks if a user can attend multiple events" do
+      user = User.create(name: 'Dev')
+      event1 = user.created_events.create(title: 'This is a test title',
+                                        description: 'This is a test description',
+                                        date: Date.today,
+                                        location: 'Nepal')
+
+      event2 = user.created_events.create(title: 'This is a test title for second event',
+                                          description: 'This is a test description for second event',
+                                          date: Date.today,
+                                          location: 'Bogota')
+      
+      attendee = User.create(name: "Miguel")
+      attending1 = attendee.attendings.create(event_id: event1.id)
+      attending2 = attendee.attendings.create(event_id: event2.id)
+
+      expect(attendee.attended_events).to eq([event2, event1])
+      expect(attendee.attendings).to eq([attending1, attending2])
+    end
+  end
 end
