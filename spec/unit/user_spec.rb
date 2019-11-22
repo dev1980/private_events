6b3396a0be1f74f6087f3158724f841906b0064b
Miguel Prada <mapra99@gmail.com>
2019-11-22 17:39:54 -0500

    fixed linter errors

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -18,30 +18,22 @@ RSpec.describe User, type: :model do
   context 'with 1 user that created 1 event' do
     it 'checks if a user can create mulitple events' do
       user = User.create(name: 'Dev')
-      event1 = user.created_events.create(title: 'This is a test title',
-                                          description: 'This is a test description',
-                                          date: Date.today,
-                                          location: 'Nepal')
+      event1 = user.created_events.create(title: 'This is a test title', description: 'This is a test description',
+                                          date: Date.today, location: 'Nepal')
 
-      event2 = user.created_events.create(title: 'This is a test title for second event',
-                                          description: 'This is a test description for second event',
-                                          date: Date.today,
-                                          location: 'Bogota')
+      event2 = user.created_events.create(title: 'This is a test title for second event', description: 'This is a test description for second event',
+                                          date: Date.today, location: 'Bogota')
 
       expect(user.created_events).to eq([event2, event1])
     end
 
     it 'checks if a user can attend multiple events' do
       user = User.create(name: 'Dev')
-      event1 = user.created_events.create(title: 'This is a test title',
-                                          description: 'This is a test description',
-                                          date: Date.today,
-                                          location: 'Nepal')
+      event1 = user.created_events.create(title: 'This is a test title', description: 'This is a test description',
+                                          date: Date.today, location: 'Nepal')
 
-      event2 = user.created_events.create(title: 'This is a test title for second event',
-                                          description: 'This is a test description for second event',
-                                          date: Date.today,
-                                          location: 'Bogota')
+      event2 = user.created_events.create(title: 'This is a test title for second event', description: 'This is a test description for second event',
+                                          date: Date.today, location: 'Bogota')
 
       attendee = User.create(name: 'Miguel')
       attending1 = attendee.attendings.create(event_id: event1.id)
