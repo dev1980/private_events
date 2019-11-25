1545bc73cc024375082b154149818a14e56cea23
Miguel Prada <mapra99@gmail.com>
2019-11-25 16:01:00 -0500

    added attend link for a user to attend an event

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root to: 'events#index'
   resources :users, only:[:new, :create, :show]
   resources :events, only:[:new, :create, :show, :index]
+  resources :attendings, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
